5346e07382c32d84b876abab197d0b3018d275f5
AlexBosio95 <alex.bosio1995@gmail.com>
2022-06-28 16:13:10 +0200

    cards

--- js/script.js
+++ js/script.js
@@ -41,11 +41,21 @@ const ourTeams = [
 const infoTeam = document.getElementById('info-team')
 
 for (let i = 0; i < ourTeams.length; i++) {
-    console.log(ourTeams[i].name)
-    console.log(ourTeams[i].role)
-    console.log(ourTeams[i].imgUrl)
-    infoTeam.innerHTML += `<div class = "text-center mt-2"> 
-    Name = ${ourTeams[i].name} 
-    Role = ${ourTeams[i].role} 
-    <img src="./img/${ourTeams[i].imgUrl}" alt="title ${ourTeams[i].imgUrl}"></div>`
+    
+    infoTeam.innerHTML += `
+    <div class="card mb-3" style="max-width: 540px;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="./img/${ourTeams[i].imgUrl}" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${ourTeams[i].name}</h5>
+          <p class="card-text">${ourTeams[i].role}</p>
+        </div>
+      </div>
+    </div>
+  </div>`
 }
+
+
