59f9330827eb5615f9fef41a74ae2dd1a6eb6873
AlexBosio95 <alex.bosio1995@gmail.com>
2022-06-28 15:48:36 +0200

    array ourTeams

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const ourTeams = [
+
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        imgUrl: 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        imgUrl: 'angela-caroll-chief-editor.jpg'
+    },
+
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        imgUrl: 'walter-gordon-office-manager.jpg'
+    },
+
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        imgUrl: 'angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        imgUrl: 'scott-estrada-developer.jpg'
+    },
+
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        imgUrl: 'barbara-ramos-graphic-designer.jpg'
+    },
+
+]
